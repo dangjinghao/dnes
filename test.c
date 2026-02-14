8792e565f54fc47ce522dc9f4a0e3be786c18da0
dangjinghao <dangjinghaoemail@163.com>
2026-02-14 09:45:31 +0000

    Simple test passed

--- test.c
+++ test.c
@@ -18,20 +18,51 @@
         NOP
         NOP
         NOP
+
+Result:
+  00: 0A
+  01: 03
+  02: 1E
 */
-static byte_t data[] = {0xA2, 0x0A, 0x8E, 0x00, 0x00, 0xA2, 0x03,
-                        0x8E, 0x01, 0x00, 0xAC, 0x00, 0x00, 0xA9,
-                        0x00, 0x18, 0x6D, 0x01, 0x00, 0x88, 0xD0,
-                        0xFA, 0x8D, 0x02, 0x00, 0xEA, 0xEA, 0xEA};
+
+byte_t fake_rom_read(addr_t addr, bool read_only) {
+  static byte_t data[] = {0xA2, 0x0A, 0x8E, 0x00, 0x00, 0xA2, 0x03,
+                          0x8E, 0x01, 0x00, 0xAC, 0x00, 0x00, 0xA9,
+                          0x00, 0x18, 0x6D, 0x01, 0x00, 0x88, 0xD0,
+                          0xFA, 0x8D, 0x02, 0x00, 0xEA, 0xEA, 0xEA};
+
+  (void)read_only;
+  return data[addr - 0x8000];
+}
+
+
+static addr_t fake_start_addr;
+byte_t fake_start_read(addr_t addr, bool read_only) {
+  (void)read_only;
+  if (addr == 0xFFFC)
+    return (byte_t)(fake_start_addr & 0xFF);
+  else if (addr == 0xFFFD)
+    return (byte_t)(fake_start_addr >> 8);
+  else
+    return 0;
+}
+
+void fake_start_register(addr_t addr) {
+  struct bus_device param = {.read = fake_start_read, .write = NULL};
+  fake_start_addr = addr;
+  bus_register(0xFFFC, 0xFFFD, &param);
+}
+
+void fake_rom_register() {
+  struct bus_device param = {.read = fake_rom_read, .write = NULL};
+  bus_register(0x8000, 0x801B, &param);
+}
+
 int main() {
-  size_t nOffset = 0x8000;
-
-  for (size_t i = 0; i < sizeof(data) / sizeof(data[0]); i++) {
-    ram[nOffset + i] = data[i];
-  }
-  ram[0xFFFC] = 0x00;
-  ram[0xFFFD] = 0x80;
-
+  fake_start_register(0x8000);
+  fake_rom_register();
+  ram_register();
+  bus_ready();
   cpu_reset();
   while (true) {
     cpu_clock();
